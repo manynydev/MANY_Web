613dd58ad18036cc1311e2e7cf3c330914ed7bba
nicseve <sevenick@gmail.com>
2021-05-15 13:07:36 -0700

    adjusted sizing of home page components
    Former-commit-id: 65a8692a12adcb9bd3d464e18260e4e62d4123fa

--- src/pages/rootHome.js
+++ src/pages/rootHome.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {NavigationButton, MainContainer, TextBaseGothic, ManyLogo} from "../components/index";
+import {NavigationButton, MainContainer, TextBaseGothic, TextBaseManySans} from "../components/index"
+import MANYLogoLarge from "../assets/many/MANY_Large.png";
+
 
 
 function RootHome() {
 
-    const aboutText = <TextBaseGothic fontSize='40px'> ABOUT </TextBaseGothic>
-    const platformText = <TextBaseGothic fontSize='40px'> PLATFORM </TextBaseGothic>
-    const videoText = <TextBaseGothic fontSize='40px'> VIDEO </TextBaseGothic>
+    const aboutText = <TextBaseManySans fontSize='40px'> About </TextBaseManySans>
+    const platformText = <TextBaseManySans fontSize='40px'> Platform </TextBaseManySans>
+    const videoText = <TextBaseManySans fontSize='40px'> Video </TextBaseManySans>
 
     return (
         <MainContainer color={'white'} flexDirection={'column'}>
-            <div style={{margin: 'auto',marginTop:300}}>
-                <ManyLogo/>
+            <div style={{margin: 'auto',marginTop:300, width:'40%'}}>
+                <img alt="logo" style={{width:'100%'}} src={MANYLogoLarge}/>
             </div>
-            <div style={{display: 'flex',gap:150, flexDirection: 'row', margin: 'auto'}}>
+            <div style={{ width: '40%',position:'relative', bottom: 150,display: 'flex',gap:150, flexDirection: 'row', margin: 'auto'}}>
                 <NavigationButton hoverBorder={true}  width={'150px'} height={'55px'} path={'/about'} displayComponent={aboutText}/>
                 <NavigationButton hoverBorder={true}  width={'230px'} height={'55px'} path={'/platform'} displayComponent={platformText}/>
                 <NavigationButton hoverBorder={true}  width={'140px'} height={'55px'} path={'/video'} displayComponent={videoText}/>
