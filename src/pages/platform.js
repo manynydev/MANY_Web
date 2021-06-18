9ee1ccc82724f2305f4165218ca467e2f6073ea2
nicseve <sevenick@gmail.com>
2021-06-17 20:47:47 -0700

    Added nav bar to root home and platform page
    Former-commit-id: 87b77cc7cc62dc46c84521c486663937237d87bc

--- src/pages/platform.js
+++ src/pages/platform.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {IphoneFrame, LeftBlackButton, MainContainer, NavButtonsContainer} from "../components/index";
+import {IphoneFrame, LeftBlackButton, MainContainer, NavButtonsContainer, TextBaseManySans} from "../components/index";
 import {CircularProgress} from "@material-ui/core";
 import leftBlackArrow from "../assets/icons/left_black.png";
 import NavigationButton from "../components/navigationButton";
@@ -9,20 +9,21 @@ function Platform() {
     const [frame1loading, setFrame1Loading] = useState(true);
     const [frame2loading, setFrame2Loading] = useState(true);
     const [framesLoading, setFramesLoading] = useState(true);
+    const aboutText = <TextBaseManySans color='white' fontSize='45px'> About </TextBaseManySans>
+    const platformText = <TextBaseManySans color='white' fontSize='45px'> Platform </TextBaseManySans>
+    const videoText = <TextBaseManySans color='white' fontSize='45px'> Video </TextBaseManySans>
 
     const leftArrowImage = <img style={{height:35,position:'absolute'}} alt='leftBlackArrow' src={leftBlackArrow}/>
     const largeNavButton = <NavigationButton hoverBorder={true} width={'50px'} height={'40px'}displayComponent={leftArrowImage} path={'/rootHome'}/>
 
     return (
         <MainContainer flexDirection={'row'}>
-            {(frame1loading || frame2loading || framesLoading) && <CircularProgress color="black"/>}
+            {(frame1loading || frame2loading || framesLoading) && <CircularProgress style={{left:30,position:'relative'}} color="black"/>}
             <div style={{display: 'flex', gap: 100, flexDirection: 'row', position: 'fixed'}}>
                 <IphoneFrame/>
                 <IphoneFrame/>
             </div>
-            <div style={{position:'relative',right:800}}>
-                {largeNavButton}
-            </div>
+
             <div onLoad={() => setFramesLoading(false)} style={{
                 zIndex: 0,
                 overflow: 'hidden',
@@ -43,6 +44,32 @@ function Platform() {
                 }}
                         style={{scrolling: 'no', height: 443, width: 250, border: 'none', overflow: 'hidden'}}
                         src={'http://localhost:3000/matching'}/>
+            </div>
+            <div style={{
+                height:70,
+                bottom:0,
+                width:'100vw',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent:'center',
+                margin: 'auto',
+                gap:150,
+                position:'absolute',
+                backgroundColor:'#0c0c0c'
+            }}>
+                <div style={{position:'relative',right:0}}>
+                    <NavigationButton hoverBorder={true} width={'165px'} height={'62px'} path={'/about'}
+                                      displayComponent={aboutText}/>
+                </div>
+                <div style={{position:'relative',left:0}}>
+                    <NavigationButton hoverBorder={true} width={'255px'} height={'62px'} path={'/platform'}
+                                      displayComponent={platformText}/>
+                </div>
+                <div style={{position:'relative',left:0}}>
+                    <NavigationButton hoverBorder={true} width={'165' +
+                    'px'} height={'62px'} path={'/video'}
+                                      displayComponent={videoText}/>
+                </div>
             </div>
         </MainContainer>
     )
