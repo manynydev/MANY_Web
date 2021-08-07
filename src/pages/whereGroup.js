a7868517fee62b409613e1efd10d0f4ee3c8d245
nicseve <sevenick@gmail.com>
2021-08-07 12:54:47 -0700

    added checks
    Former-commit-id: 672d1ad2887ddef2a71efcca206d2cf139eb5f60

--- src/pages/whereGroup.js
+++ src/pages/whereGroup.js
@@ -19,12 +19,24 @@ function WhereGroup() {
     const [navProps, setNavProps] = useState(location.navProps);
     const [selectedCheck, selectCheck] = useState(0);
 
+    function getSelectedItem() {
+        switch(selectedCheck){
+            case 1:
+                return 95
+            case 2:
+                return 125
+            default:
+                return 65
+        }
+    }
+
     return (
         <MainContainer>
             <IphoneScreen>
                 <div style={{margin: '15px'}}>
                     <TextHeaderCush>Where is the group?</TextHeaderCush>
                 </div>
+                <img style={{position:'absolute',left:32, height:20, top: getSelectedItem()}} src={require('../assets/icons/check_black.png').default} alt={'Check'} />
                 <div
                     style={{
                         display: 'flex',
@@ -34,9 +46,9 @@ function WhereGroup() {
                         left: '15px',
                         gap: '5px'
                     }}>
-                    <Button style={{width:155,height:25}}><TextBaseManySans fontSize='20px'>__ Located in</TextBaseManySans></Button>
-                    <Button style={{width:195,height:25}}><TextBaseManySans fontSize='20px'>__ Traveling from</TextBaseManySans></Button>
-                    <Button style={{width:175,height:25}}><TextBaseManySans fontSize='20px'>__ Sharing from</TextBaseManySans></Button>
+                    <Button onClick={() => {selectCheck(0)}} style={{width:155,height:25}}><TextBaseManySans fontSize='20px'>__ Located in</TextBaseManySans></Button>
+                    <Button onClick={() => {selectCheck(1)}} style={{width:195,height:25}}><TextBaseManySans fontSize='20px'>__ Traveling from</TextBaseManySans></Button>
+                    <Button onClick={() => {selectCheck(2)}} style={{width:175,height:25}}><TextBaseManySans fontSize='20px'>__ Sharing from</TextBaseManySans></Button>
                 </div>
                 <div
                     style={{display: 'flex', flexDirection: 'column', position: 'relative', top: '20px', left: '25px'}}>
