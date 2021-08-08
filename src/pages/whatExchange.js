440d285cc5a3cd393689c57edec57cb91d59b387
nicseve <sevenick@gmail.com>
2021-08-07 19:18:23 -0700

    finished positioning of exchange page elements
    Former-commit-id: 6cdab813f8213a4ddf718069827c447f5673cd05

--- src/pages/whatExchange.js
+++ src/pages/whatExchange.js
@@ -11,9 +11,11 @@ import {useHistory} from "react-router-dom";
 function WhatExchange(props) {
     let history = useHistory();
     const title = <TextBaseManySans color={'#919191'} underline={true} fontSize='16px'> select title </TextBaseManySans>
-    const describeActivities = <TextBaseManySans color={'#919191'} underline={true} fontSize='16px'> describe
-        activities </TextBaseManySans>
-    const selectOffers = <TextBaseManySans color={'#919191'} underline={true} fontSize='16px'> select offers </TextBaseManySans>
+    const describe = <TextBaseManySans color={'#919191'} underline={true} fontSize='16px'> describe </TextBaseManySans>
+    const activities = <TextBaseManySans color={'#919191'} underline={true}
+                                         fontSize='16px'> activities </TextBaseManySans>
+    const selectOffers = <TextBaseManySans color={'#919191'} underline={true} fontSize='16px'> select
+        offers </TextBaseManySans>
 
 
     return (
@@ -27,26 +29,36 @@ function WhatExchange(props) {
                     to
                     engage</TextBaseCush>
 
-                <div style={{top: '90px',left:'2px', position: 'absolute'}}>
+                <div style={{top: '82px', left: '2px', position: 'absolute'}}>
                     <NavigationButton width={'129px'} displayComponent={title} path={'/whatDurationMonths'}
                                       height={'16px'}/>
                 </div>
-                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '77px', left: '107px'}}
+                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '70px', left: '107px'}}
                               fontSize='18px'> to </TextBaseCush>
-                <div style={{top: '115px', position: 'absolute'}}>
-                    <NavigationButton width={'190px'} displayComponent={describeActivities} path={'/whatDurationMonths'}
+
+                <div style={{top: '82px', left: '135px', position: 'absolute'}}>
+                    <NavigationButton width={'80px'} displayComponent={describe} path={'/whatDurationMonths'}
                                       height={'16px'}/>
                 </div>
-                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '103px', left: '160px'}}
+
+                <div style={{top: '100px', left: '17px', position: 'absolute'}}>
+                    <NavigationButton width={'80px'} displayComponent={activities} path={'/whatDurationMonths'}
+                                      height={'16px'}/>
+                </div>
+
+                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '88px', left: '85px'}}
                               fontSize='18px'>.</TextBaseCush>
-                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '127px'}}
+
+                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '88px',left:'90px'}}
                               fontSize='18px'>The group offers</TextBaseCush>
-                <div style={{top: '145px',left:'130px', position: 'absolute'}}>
+
+                <div style={{top: '119px', position: 'absolute'}}>
                     <NavigationButton width={'139px'} displayComponent={selectOffers} path={'/whatDurationMonths'}
                                       height={'16px'}/>
                 </div>
 
-
+                <TextBaseCush fontWeight='normal' style={{margin: '15px', top: '108px', left: '110px'}}
+                              fontSize='18px'>.</TextBaseCush>
 
 
                 <NavButtonsContainer>
