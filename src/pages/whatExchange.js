5d5571a76ac37d7249f0e2806ce3515976ba1e7b
nicseve <sevenick@gmail.com>
2021-08-08 13:43:03 -0700

    added custom text input
    Former-commit-id: 3641af0bada16e2b9e275b059f832776a6d967f7

--- src/pages/whatExchange.js
+++ src/pages/whatExchange.js
@@ -7,12 +7,31 @@ import {
     LeftBlackButton, ManyHomeButton, RightBlackButton, NavButtonsContainer, NavigationButton, TextBaseManySans
 } from "../components";
 import {useHistory} from "react-router-dom";
+import Input from '@material-ui/core/Input';
+import {withStyles} from "@material-ui/core";
+
+const StyledInput = withStyles({
+    root: {
+        fontFamily: 'ManySans, sans-serif',
+        fontSize: '15px',
+        fontWeight:'bold'
+    },
+    underline: {
+        '&:before': {
+            marginBottom: 7
+        },
+        '&:after': {
+            borderBottomColor: 'black', marginBottom: 7
+        }
+    }
+})(Input);
+
 
 function WhatExchange(props) {
     let location = useHistory().location;
     console.log(location);
-    const jobTitle = <TextBaseManySans color={'#919191'} underline={true} fontSize='15px'> {location?.navProps?.job? location.navProps.job : 'select title'} </TextBaseManySans>
-    const describeActivities = <TextBaseManySans color={'#919191'} underline={true} fontSize='15x'> describe activites </TextBaseManySans>
+    const jobTitle = <TextBaseManySans color={'#919191'} underline={true}
+                                       fontSize='15px'> {location?.navProps?.job ? location.navProps.job : 'select title'} </TextBaseManySans>
     const selectOffers = <TextBaseManySans color={'#919191'} underline={true} fontSize='15px'> select
         offers </TextBaseManySans>
 
@@ -23,32 +42,41 @@ function WhatExchange(props) {
                 <div style={{margin: '15px'}}>
                     <TextHeaderCush>What is the exchange?</TextHeaderCush>
                 </div>
-                <div style={{display:'flex',flexDirection:'row',top:'50px',position:'absolute',flexWrap:"wrap",left:'5px',margin:'15px'}}>
-                    <TextBaseCush fontWeight='normal' style={{position:'relative'}} fontSize='16px'> The group needs
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    top: '50px',
+                    position: 'absolute',
+                    flexWrap: "wrap",
+                    left: '5px',
+                    margin: '15px'
+                }}>
+                    <TextBaseCush fontWeight='normal' style={{position: 'relative'}} fontSize='16px'> The group needs
                         to
                         engage
                     </TextBaseCush>
-                    <div style={{position:'relative',right:'7px',bottom:'5px'}}>
-                        <NavigationButton width={jobTitle.props.children.length} navProps={location?.navProps} displayComponent={jobTitle} path={'/jobs'}
+                    <div style={{position: 'relative', right: '7px', bottom: '5px'}}>
+                        <NavigationButton width={jobTitle.props.children.length} navProps={location?.navProps}
+                                          displayComponent={jobTitle} path={'/jobs'}
                                           height={'16px'}/>
                     </div>
-                    <TextBaseCush fontWeight='normal' style={{position:'relative',bottom:'1px',right:'10px'}}
+                    <TextBaseCush fontWeight='normal' style={{position: 'relative', bottom: '1px', right: '10px'}}
                                   fontSize='16px'> to </TextBaseCush>
 
-                    <div style={{position:'relative',right:'7px',bottom:'6px' }}>
-                        <NavigationButton width={(describeActivities.props.children.length)} displayComponent={describeActivities} path={'/whatDurationMonths'}
-                                          height={'16px'}/>
+                    <div style={{position: 'relative', right: '0px', bottom: '6px'}}>
+                        <StyledInput style={{width:150}} placeholder={'describe activities'} multiline={true}/>
                     </div>
-                    <TextBaseCush fontWeight='normal' style={{position:'relative',bottom:'4px',right:'15px'}}
+                    <TextBaseCush fontWeight='normal' style={{position: 'relative', bottom: '-1px', right: '2px'}}
                                   fontSize='16px'>.</TextBaseCush>
-                    <TextBaseCush fontWeight='normal' style={{position:'relative',bottom:'5px'}}
+                    <TextBaseCush fontWeight='normal' style={{position: 'relative', bottom: '5px'}}
                                   fontSize='16px'>The group offers</TextBaseCush>
-                    <div style={{position:'relative',bottom:'8px',right:'5px'}}>
-                        <NavigationButton width={selectOffers.props.children.length} navProps={location?.navProps} displayComponent={selectOffers} path={'/offers'}
+                    <div style={{position: 'relative', bottom: '8px', right: '5px'}}>
+                        <NavigationButton width={selectOffers.props.children.length} navProps={location?.navProps}
+                                          displayComponent={selectOffers} path={'/offers'}
                                           height={'16px'}/>
                     </div>
 
-                    <TextBaseCush fontWeight='normal' style={{position:'relative',right:'12px',bottom:'3px'}}
+                    <TextBaseCush fontWeight='normal' style={{position: 'relative', right: '12px', bottom: '3px'}}
                                   fontSize='16px'>.</TextBaseCush>
                 </div>
 
