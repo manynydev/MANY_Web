31ad76176f610c49fd84cd546883295b8cfb1536
nicseve <sevenick@gmail.com>
2021-08-15 11:17:03 -0700

    Fixed duration months formatting
    Former-commit-id: 50898a8e336b4ecb152a3fb73ebc34c4be4c415b

--- src/pages/whatDurationMonths.js
+++ src/pages/whatDurationMonths.js
@@ -1,63 +1,68 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
     MainContainer,
     IphoneScreen,
     TextBaseGothic,
     RightWhiteButton
 } from "../components";
-import {SnapList, SnapItem, useDragToScroll} from "react-snaplist-carousel";
-import {useLocation} from "react-router-dom";
+import {SnapList, SnapItem, useDragToScroll,useScroll} from "react-snaplist-carousel";
 
 
-function createMonthsElements() {
-    let months = [];
-    for (let i = 1; i <= 24; i++) {
-        months.push(
-            <SnapItem margin={{top: '10px', bottom: '10px'}} height="80px" snapAlign="center">
-                <TextBaseGothic
-                    style={{
-                        color: 'white',
-                        fontWeight: 'regular',
-                        fontSize: '72px',
-                        // paddingBottom: i === 24 ? 395 : 0,
-                        // paddingTop: i === 1 ? 40 : 0
-                    }}>{i}
-                </TextBaseGothic>
-            </SnapItem>)
-    }
-    return months
-}
 
-//TODO: Dynamically adjust months text margin left when month int is not double digit (0-9)
+
 
 const WhatDurationMonths = () => {
-    const location = useLocation();
-    const [navProps, setNavProps] = React.useState(location.navProps);
 
     const snapList = useRef(null);
-    useDragToScroll({ref: snapList, disable: false});
+    const lastSnapItem = useRef(null);
+    const goToSnapItem = useScroll({ ref: snapList });
+    useDragToScroll({ref:snapList});
+
+    useEffect(() => {
+        goToSnapItem(14);
+        console.log(lastSnapItem.current?.focus());
+        console.log(snapList.current?.children);
+    },[])
+
+    function createMonthsElements() {
+        let months = [];
+        for (let i = 0; i <= 29; i++) {
+            months.push(
+                <SnapItem margin={{top: '2px'}} height="80px" snapAlign="center">
+                    <TextBaseGothic
+                        onClick={() => goToSnapItem(i+2)}
+                        style={{
+                            color: `${ i > 24 || i === 0 ? '#161616' :'white'}`,
+                            fontWeight: 'regular',
+                            fontSize: '72px',
+                        }}>{i}
+                    </TextBaseGothic>
+                </SnapItem>)
+        }
+        return months
+    }
 
     const months = createMonthsElements();
-
     return (
         <MainContainer>
             <IphoneScreen style={{backgroundColor: '#161616'}}>
                 <TextBaseGothic style={{
                     position: 'absolute',
                     color: 'white',
-                    marginLeft: '120px',
-                    marginTop: 105,
+                    marginLeft: '105px',
+                    marginTop: 121,
                     fontWeight: 'regular',
                     fontSize: '15px'
                 }}>months
                 </TextBaseGothic>
                 <div style={{marginLeft: '20px', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <SnapList width='70px' height='538px' direction="vertical" ref={snapList}>
+                    <SnapList width='10px' height='538px' direction="vertical" ref={snapList}>
                         {months}
                     </SnapList>
                 </div>
-                <RightWhiteButton style={{position: 'relative', left: 188, bottom: 133, height: '19px'}}
-                                  path={'/whatDuration'}/>
+                <div style={{position: 'absolute', right: -10, bottom: 15}}>
+                    <RightWhiteButton path={'/whatDuration'}/>
+                </div>
             </IphoneScreen>
         </MainContainer>
     )
