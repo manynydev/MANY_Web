907b1cbe8e7d0bc565bbf65cbeac912e920e3df2
manynydev <manynydev@gmail.com>
2021-10-24 10:43:02 -0700

    Added gh pages support

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename="/MANY_Web">
             <App/>
         </Router>
     </React.StrictMode>,
